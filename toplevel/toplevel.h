d8da885d50f570ae30220e11589580a572090322
Matthieu Moy <Matthieu.Moy@imag.fr>
2010-03-14 22:16:38 +0100

    forgotten files for toplevel/

---
+++ toplevel/toplevel.h
@@ -0,0 +1,18 @@
+/********************************************************************
+ * Copyright (C) 2010 by Verimag                                    *
+ * Initial author: Matthieu Moy                                     *
+ * This file must not be distributed outside Verimag                *
+ ********************************************************************/
+
+/*!
+  \file toplevel.h
+  \brief Toplevel calling the front-end and back-ends.
+
+  
+*/
+#ifndef TOPLEVEL_H
+#define TOPLEVEL_H
+
+int toplevel_main(int argc, char **argv);
+
+#endif // TOPLEVEL_H
