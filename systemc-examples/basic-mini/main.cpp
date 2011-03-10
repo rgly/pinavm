f3393e082c3bee902a016312abe02375e7de5d21
simo <lamraoui@hotmail.fr>
2011-03-10 03:15:13 +0100

    Adding tweto_call_process_method function to allow process optimization in the Tweto backend

--- systemc-examples/basic-mini/main.cpp
+++ systemc-examples/basic-mini/main.cpp
@@ -13,6 +13,12 @@
 #include "basic.h"
 #include "bus.h"
 
+// FIXME: move this code in a bytecode library
+extern "C" void tweto_call_process_method(sc_core::SC_ENTRY_FUNC vfct, sc_core::sc_process_host *host);
+void tweto_call_process_method(sc_core::SC_ENTRY_FUNC vfct, sc_core::sc_process_host *host) {
+    (host->*vfct)();
+}
+
 using namespace std;
 using namespace sc_core;
 
