dd6267c5a99d956dea5112cf564769412c042274
simo <lamraoui@hotmail.fr>
2011-05-23 11:09:39 +0200

    Adding a test
    - A simple test with two initiators and one target

---
+++ systemc-examples/basic-multiinitiator/main.cpp
@@ -0,0 +1,86 @@
+/********************************************************************
+ * Copyright (C) 2009 by Ensimag and Verimag                        *
+ * Initial author: Matthieu Moy                                     *
+ ********************************************************************/
+
+/* Plateforme SystemC utilisant le protocole TLM BASIC.
+
+   Cette version est en un seul fichier pour faciliter la
+   compréhension. Une version multi-fichiers (donc plus propre) est
+   également disponible.
+*/
+
+#include "basic.h"
+#include "bus.h"
+
+// FIXME: move this code in a bytecode library
+extern "C" void tweto_call_process_method(sc_core::SC_ENTRY_FUNC vfct, sc_core::sc_process_host *host);
+void tweto_call_process_method(sc_core::SC_ENTRY_FUNC vfct, sc_core::sc_process_host *host) {
+    (host->*vfct)();
+}
+
+using namespace std;
+using namespace sc_core;
+
+struct initiator : sc_module {
+        basic::initiator_socket<initiator> socket;
+        void thread(void) {
+		basic::addr_t addr = 4;
+        cout << name() << " write to Bob at address " << addr << endl;
+		for (basic::data_t val = 1; val <= 10; val++) {
+			val++;
+                        socket.write(addr, val);
+                }
+        }
+        SC_CTOR(initiator) {
+                SC_THREAD(thread);
+        }
+};
+
+struct target : sc_module, basic::target_module_base {
+        basic::target_socket<target> socket;
+        tlm::tlm_response_status write(const basic::addr_t &a,
+                                       const basic::data_t &d) {
+                cout << name() << " à reçu : " << d << endl;
+                return tlm::TLM_OK_RESPONSE;
+        }
+        tlm::tlm_response_status read (const basic::addr_t &a,
+                                       /* */ basic::data_t &d) {
+                SC_REPORT_ERROR("TLM", "non implémenté");
+                abort();
+        }
+        SC_CTOR(target) {/* */}
+};
+
+int sc_main (int argc, char ** argv) {
+        /*
+         +---------+     +-------------+    +--------+
+         |        +++   +-+           +++  +++       |
+         | Alice  | +---+ |  Router   | +--+ |  Bob  |
+         |        +++ | +-+           +++  +++       |
+         +---------+  |  +-------------+    +--------+
+                      |
+         +---------+  |
+         |        +++ |
+         | Lucie  | +-
+         |        +++ 
+         +---------+ 
+         */
+
+        initiator a1("Alice");
+        initiator a2("Lucie");
+        target b("Bob");
+        Bus router("Router");
+
+        /* Bob is mapped at addresses [0, 100[, i.e. ... */
+        router.map(b.socket, 0, 100);
+
+        /* connect components to the bus */
+        a1.socket.bind(router.target);
+        a2.socket.bind(router.target);
+        router.initiator.bind(b.socket);
+
+        /* and start simulation */
+        sc_start();
+        return 0;
+}
