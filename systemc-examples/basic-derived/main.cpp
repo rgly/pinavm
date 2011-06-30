208406b49f23696594cd59152725fff3ace4f497
simo <lamraoui@hotmail.fr>
2011-06-30 14:40:35 +0200

    Adding a new test
    - Test the target method retrieving with derived classes.

---
+++ systemc-examples/basic-derived/main.cpp
@@ -0,0 +1,102 @@
+/********************************************************************
+ * Copyright (C) 2009 by Ensimag and Verimag                        *
+ * Initial author: Matthieu Moy                                     *
+ ********************************************************************/
+
+/* Plateforme SystemC utilisant le protocole TLM BASIC.
+
+   Cette version est en un seul fichier pour faciliter la
+   compréhension. Une version multi-fichiers (donc plus propre) est
+   également disponible.
+*/
+
+#include "basic.h"
+#include "bus.h"
+
+// FIXME: move this code in a bytecode library
+extern "C" void tweto_call_process_method(sc_core::SC_ENTRY_FUNC vfct, sc_core::sc_process_host *host);
+void tweto_call_process_method(sc_core::SC_ENTRY_FUNC vfct, sc_core::sc_process_host *host) {
+    (host->*vfct)();
+}
+extern "C" void tweto_call_write_method(basic::target_module_base *module, 
+                                        basic::addr_t a, basic::data_t d);
+void tweto_call_write_method(basic::target_module_base *module, 
+                             basic::addr_t a, basic::data_t d) {
+    module->write(a, d);
+}
+extern "C" void tweto_call_read_method(basic::target_module_base *module, 
+                                       basic::addr_t a, basic::data_t d);
+void tweto_call_read_method(basic::target_module_base *module, 
+                            basic::addr_t a, basic::data_t d) {
+    module->read(a, d);
+}
+
+using namespace std;
+using namespace sc_core;
+
+
+struct initiator : sc_module {
+        basic::initiator_socket<initiator> socket;
+        void thread(void) {
+            basic::data_t val = 16;
+            basic::addr_t addr = 4;
+            socket.write(addr, val);
+            
+        }
+        SC_CTOR(initiator) {
+                SC_THREAD(thread);
+        }
+};
+
+
+class target : public basic::target_module_base {
+public:
+
+    tlm::tlm_response_status write(const basic::addr_t &a,
+                                   const basic::data_t &d) {
+        cout << "j'ai reçu : " << d << endl;
+        return tlm::TLM_OK_RESPONSE;
+    }
+    
+    tlm::tlm_response_status read (const basic::addr_t &a,
+                                   /* */ basic::data_t &d) {
+        SC_REPORT_ERROR("TLM", "non implémenté");
+        abort();
+    }
+    
+};
+
+class DerivedTarget : public sc_module, public target {
+public:
+
+    basic::target_socket<DerivedTarget> socket;
+    
+    SC_CTOR(DerivedTarget) { /* */ }
+    
+};
+
+
+int sc_main (int argc, char ** argv) {
+        /*
+         +---------+    +-------------+    +--------+
+         |        +++  +-+           +++  +++       |
+         | Alice  | +--+ |  Router   | +--+ |  Bob  |
+         |        +++  +-+           +++  +++       |
+         +---------+    +-------------+    +--------+
+         */
+
+        initiator a("Alice");
+        DerivedTarget b("Bob");
+        Bus router("Router");
+
+        /* Bob is mapped at addresses [0, 100[, i.e. ... */
+        router.map(b.socket, 0, 100);
+
+        /* connect components to the bus */
+        a.socket.bind(router.target);
+        router.initiator.bind(b.socket);
+
+        /* and start simulation */
+        sc_start();
+        return 0;
+}
