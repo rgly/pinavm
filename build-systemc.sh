3628bfd2d56698e0e6a577a8a0c3e0abadd3894c
Chia-Wei Chang <rglyab@gmail.com>
2013-04-10 20:39:54 +0800

    Remove out-of-date installation shell script
    Since most of installation scripts are out-of-date and have substitute
    in cmake, those code or scripts have little chance to be used again.
    
    Keep config.mk.in because systemc-examples/* are still relying
    MakeFile to launch testcases. Modify config.sh.in to export
    LLVM_COMPILER and LLVMC_FLAGS. Users can simply "source config.sh"
    to get handy environment variables.

--- build-systemc.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-if [ ! -f external/systemc-2.2.0/objdir-gcc/Makefile ]; then 
-    cd external/systemc-2.2.0/ && ./autogen.sh && 
-    mkdir -p objdir-gcc/ && cd objdir-gcc/ && 
-    ../configure &&
-    cd ../../../
-fi
-cd external/systemc-2.2.0/objdir-gcc/ && $MAKE pthreads_debug
