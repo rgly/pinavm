72be75f16284b756fbdeea26b6f40d37a6ad6831
Chia-Wei Chang <rglyab@gmail.com>
2013-04-24 17:38:49 +0800

    Remove tweto_call_method from testcases
    Since Tweto backend call those method from external library,
    We don't need to specify it in source code.

--- systemc-examples/rendezvous/sc_main.cpp
+++ systemc-examples/rendezvous/sc_main.cpp
@@ -3,13 +3,6 @@
 #include "Consommateur.h"
 #include "RendezVous.h"
 
-
-// FIXME: move this code in a bytecode library
-extern "C" void tweto_call_process_method(sc_core::SC_ENTRY_FUNC vfct, sc_core::sc_process_host *host);
-void tweto_call_process_method(sc_core::SC_ENTRY_FUNC vfct, sc_core::sc_process_host *host) {
-    (host->*vfct)();
-}
-
 using namespace std;
 
 int sc_main(int, char**)
