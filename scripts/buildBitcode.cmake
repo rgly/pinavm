dfb116a32c332dca9eb3275e1107a7937fc9315c
Chia-Wei Chang <rglyab@gmail.com>
2014-08-16 17:21:31 +0800

    parse header dependency in buildBitcode()
    Use clang -MM to generate these dependencies. it slow down CMake
    configuration time by 12 seconds. I should find other ways to
    do it on build stage.

--- scripts/buildBitcode.cmake
+++ scripts/buildBitcode.cmake
@@ -4,6 +4,42 @@
 
 SET(LLVM_EXT bc)
 
+macro(collect_included_headers f_abso_temp_src)
+  # collect the header dependency of source code.
+  execute_process(COMMAND 
+    ${LLVM_COMPILER} ${LLVMC_FLAGS} ${f_abso_temp_src} -MM
+    OUTPUT_VARIABLE f_dep_var
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+  # clang -MM generate Makefile dep, so string truncation is necessery.
+  if(NOT "${f_dep_var}" STREQUAL "")
+    # remove the backslash and newline
+    string(REPLACE "\\\n" ";" f_dep_var "${f_dep_var}")
+    string(REPLACE " "    ""  f_dep_var "${f_dep_var}")
+    # remove the first item, which normally be "main.o : main.cpp"
+    LIST(REMOVE_AT f_dep_var 0)
+  endif()
+endmacro(collect_included_headers)
+
+macro(build_llvm_bc_object f_temp_src)
+  get_filename_component(f_abso_temp_src ${f_temp_src} ABSOLUTE)
+
+  STRING(REPLACE ".cpp" ".s" f_temp_object ${f_abso_temp_src})
+  STRING(REPLACE ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_BINARY_DIR} 
+      	   f_temp_object ${f_temp_object})
+
+  # the macro to create header dependency.
+  collect_included_headers(${f_abso_temp_src})
+
+  LIST(APPEND f_dep_var ${f_abso_temp_src})
+
+  add_custom_command(OUTPUT ${f_temp_object} DEPENDS ${f_dep_var}
+    COMMAND ${LLVM_COMPILER} ${LLVMC_FLAGS} ${f_abso_temp_src}
+    -o ${f_temp_object} VERBATIM)
+
+  LIST(APPEND f_objects ${f_temp_object})
+endmacro(build_llvm_bc_object)
+
 function(build_llvm_bitcode f_target_name f_src_list)
   set(f_target_unopt_file
              ${CMAKE_CURRENT_BINARY_DIR}/${f_target_name}.unopt.${LLVM_EXT})
@@ -15,18 +51,7 @@ function(build_llvm_bitcode f_target_name f_src_list)
 
   # for each .cpp file, LLVMC would generate .s file in binary dir.
   foreach(f_temp_src ${${f_src_list}})
-    get_filename_component(f_abso_temp_src ${f_temp_src} ABSOLUTE)
-
-    STRING(REPLACE ".cpp" ".s" f_temp_object ${f_abso_temp_src})
-    STRING(REPLACE ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_BINARY_DIR} 
-		   f_temp_object ${f_temp_object})
-
-    add_custom_command(OUTPUT ${f_temp_object} DEPENDS ${f_abso_temp_src}
-      COMMAND ${LLVM_COMPILER} ${LLVMC_FLAGS} ${f_abso_temp_src}
-      -o ${f_temp_object}
-      VERBATIM)
-
-    LIST(APPEND f_objects ${f_temp_object})
+    build_llvm_bc_object(${f_temp_src})
   endforeach(f_temp_src)
 
 
