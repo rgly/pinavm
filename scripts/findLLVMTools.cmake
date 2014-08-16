2b4867ebe2b86c5b5405979aebbbb6eaccffc0f1
Chia-Wei Chang <rglyab@gmail.com>
2014-08-16 12:51:57 +0800

    find LLVM_OPT in CMake script
    I make the find-LLVM_LINK script into a handy macro. It will be
    easy to find new LLVM tools. I also improve the CMake status
    messages about LLVM tools.

--- scripts/findLLVMTools.cmake
+++ scripts/findLLVMTools.cmake
@@ -1,7 +1,15 @@
-find_program(LLVM_LINK "llvm-link-${LLVM_RECOMMAND_VERSION}"
-		NAMES llvm-link
+macro(findLLVMTool var_name tool_name)
+  find_program(${var_name} "${tool_name}-${LLVM_RECOMMAND_VERSION}"
+		NAMES ${tool_name}
 		HINTS ${LLVM_ROOT}/bin)
 
-if(${LLVM_LINK} STREQUAL "LLVM_LINK-NOTFOUND")
-  message(FATAL_ERROR "finds no LLVM_LINK")
-endif()
+  if(${${var_name}} STREQUAL "${var_name}-NOTFOUND")
+    message(FATAL_ERROR "finds no ${var_name}")
+  else()
+    message(STATUS "${var_name} found : ${${var_name}}")
+  endif()
+endmacro(findLLVMTool)
+
+
+findLLVMTool(LLVM_LINK llvm-link)
+findLLVMTool(LLVM_OPT opt)
