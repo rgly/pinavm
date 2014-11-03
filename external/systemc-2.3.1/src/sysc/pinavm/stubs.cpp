492f62b28638d7ab0e81b008db1dac2adeb27c82
Chia-Wei Chang <rglyab@gmail.com>
2014-11-03 19:34:34 +0800

    add permalloc for static tweto usage
    permalloc allocates (when initialized) from a preallocated array, like
    __gnu_cxx::array_allocator does
    
    A Systemc-2.3.1 port of Systemc-2.3.0 commit
    8320394e96abb2c26f4fa5c4b09f7767001f168b
    This patch may be a little different because I do not apply
    39436e9517681d55ba6807b98c137fc61ced9bcf

---
+++ external/systemc-2.3.1/src/sysc/pinavm/stubs.cpp
@@ -0,0 +1,42 @@
+/* This file defines weak symbols to enable the embedded SystemC library to be used
+   without PinaVM. Define NO_WEAK_SYMBOLS to disable them, in which case this SystemC
+   library will need either PinaVM or a strong-symbol version of this file to work. */
+
+#ifndef NO_WEAK_SYMBOLS
+#define WEAK __attribute__((weak))
+
+#include "systemc"
+#include <assert.h>
+
+extern "C" WEAK void relink_processes (void)
+{
+}
+
+extern "C" WEAK void pinavm_callback (sc_core::sc_simcontext* sc, const sc_core::sc_time& d)
+{
+	relink_processes ();
+	sc->simulate (d);
+}
+
+void WEAK tweto_mark_const (void const* x, unsigned long y)
+{
+}
+
+// C++ linkage function
+int launch_systemc (int, char**);
+
+extern "C" WEAK int main (int argc, char* argv[])
+{
+	launch_systemc (argc, argv);
+}
+
+// necessary to run lli with LD_PRELOAD of the internal SystemC lib
+extern "C" WEAK int sc_main (int, char**)
+{
+	assert (0);
+}
+
+bool WEAK disable_debug_msg;
+
+#endif
+
