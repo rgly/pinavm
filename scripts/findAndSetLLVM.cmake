bf35cef86630494c80125937e976b621de7bf0f4
Chia-Wei Chang <rglyab@gmail.com>
2015-05-03 10:34:12 +0800

    code cleanup
    Make findLLVM.cmake & findLLVMTools.cmake a pure collection of
    functions.
    
    This patch does not change the behavior of the CMake script.

---
+++ scripts/findAndSetLLVM.cmake
@@ -0,0 +1,42 @@
+set(SCRIPT_DIR "${CMAKE_SOURCE_DIR}/scripts")
+include(${SCRIPT_DIR}/findLLVM.cmake)
+include(${SCRIPT_DIR}/findLLVMTools.cmake)
+include(${SCRIPT_DIR}/buildBitcode.cmake)
+
+# This function returns these variable
+#
+# LLVM_ROOT
+# LLVM_LIBS
+# LLVM_DEFINITIONS
+# LLVM_INCLUDE_DIRS
+# LLVM_LIBRARY_DIRS
+FIND_LLVM(${LLVM_ROOT} ${LLVM_RECOMMAND_VERSION} ${AUTOINSTALL})
+findLLVMTool(LLVM_LINK llvm-link)
+findLLVMTool(LLVM_OPT opt)
+findLLVMTool(LLVM_COMPILER clang++)
+
+
+SET(LLVMC_INCLUDE_DIR
+               "-I${CMAKE_SOURCE_DIR}/external/systemc-${SYSTEMC_VERSION}/src/"
+               "-I${CMAKE_SOURCE_DIR}/external/basic")
+set(LLVMC_FLAGS -emit-llvm ${LLVM_DEFINITIONS} ${LLVMC_INCLUDE_DIR}
+	-std=c++0x -fno-inline-functions -fno-use-cxa-atexit -c )
+
+# Use settings from LLVM cmake module or llvm-config.
+include_directories( ${LLVM_INCLUDE_DIRS} )
+link_directories( ${LLVM_LIBRARY_DIRS} )
+add_definitions( ${LLVM_DEFINITIONS} )
+# probably some llvm-config work to do...
+SET(REQ_LLVM_LIBRARIES ${LLVM_LIBS})
+
+# For debug use only
+if(${TEST_CMAKE})
+  message("debug messages below")
+  message("LLVM_ROOT is ${LLVM_ROOT}")
+  message("REQ_LLVM_LIBRARIES is ${REQ_LLVM_LIBRARIES}")
+  message("LLVM_COMPILER is ${LLVM_COMPILER}")
+  message("LLVMC_FLAGS is ${LLVMC_FLAGS}")
+  message("LLVM_DEFINITIONS is ${LLVM_DEFINITIONS}")
+  message("LLVM_LIBRARY_DIRS is ${LLVM_LIBRARY_DIRS}")
+  message("LLVM_INCLUDE_DIRS is ${LLVM_INCLUDE_DIRS}")
+endif()
--- scripts/findLLVMTools.cmake
+++ scripts/findLLVMTools.cmake
@@ -1,20 +1,18 @@
-macro(findLLVMTool var_name tool_name)
-  find_program(${var_name} "${tool_name}-${LLVM_PATCH_VERSION}"
-		NAMES "${tool_name}-${LLVM_RECOMMAND_VERSION}" ${tool_name}
-		HINTS ${LLVM_ROOT}/bin)
+FUNCTION(findLLVMTool_Fct var_name tool_name LLVM_ROOT LLVM_RECOMMAND_VERSION)
+  set(LLVM_BIN_DIR "${LLVM_ROOT}/bin")
+  find_program(${var_name}
+  NAMES "${LLVM_BIN_DIR}/${tool_name}-${LLVM_RECOMMAND_VERSION}"
+        "${LLVM_BIN_DIR}/${tool_name}")
 
   if(${${var_name}} STREQUAL "${var_name}-NOTFOUND")
     message(FATAL_ERROR "finds no ${var_name}")
   else()
     message(STATUS "${var_name} found : ${${var_name}}")
   endif()
-endmacro(findLLVMTool)
 
-findLLVMTool(LLVM_LINK llvm-link)
-findLLVMTool(LLVM_OPT opt)
-findLLVMTool(LLVM_COMPILER clang++)
-SET(LLVMC_INCLUDE_DIR
-               "-I${CMAKE_SOURCE_DIR}/external/systemc-${SYSTEMC_VERSION}/src/"
-               "-I${CMAKE_SOURCE_DIR}/external/basic")
-set(LLVMC_FLAGS -emit-llvm ${LLVM_DEFINITIONS} ${LLVMC_INCLUDE_DIR}
-	-std=c++0x -fno-inline-functions -fno-use-cxa-atexit -c )
+  set(var_name ${var_name} PARENT_SCOPE)
+ENDFUNCTION(findLLVMTool_Fct)
+
+macro(findLLVMTool var_name tool_name)
+  findLLVMTool_Fct(${var_name} ${tool_name} ${LLVM_ROOT} ${LLVM_RECOMMAND_VERSION})
+endmacro(findLLVMTool)
